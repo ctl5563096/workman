79a8ce51e288b6eaec14b9bb2a56bdda2ec1ee68
shy <chentulin@163.com>
2021-03-06 11:28:42 +0800

    feat:修改域名地址

--- Applications/YourApp/Events.php
+++ Applications/YourApp/Events.php
@@ -69,6 +69,7 @@ class Events
    {
        var_export($message. '客户端id' . $client_id);
        $req_data = json_decode($message, true);
+       var_dump($req_data);die();
        Gateway::sendToClient($client_id, $req_data['content']);
 //       var_export($req_data);
        // 如果是向某个客户端发送消息
